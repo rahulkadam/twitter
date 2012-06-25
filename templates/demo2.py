3c90263c173e164dddc63a607f46fccc6fde58bd
rahul <rahul@rahul.(none)>
2012-06-25 06:12:54 -0800

    ADDING FIRST FILE

---
+++ templates/demo2.py
@@ -0,0 +1,30 @@
+import flask,flask.views
+app=flask.Flask(__name__)
+
+class View(flask.views.MethodView):
+    def get(self):
+        return flask.render_template("login.html")
+    def post(self):
+        return str(flask.request.form["name"])+str(flask.request.form["name1"])
+
+class log(flask.views.MethodView):
+    def get(self):
+        return flask.render_template("login.html")
+        #return 'helooo'  
+    def post(self,page1="loginhgjfdj"):
+        return page1
+#        return flask.render_template("rating.html")
+class rating(flask.views.MethodView):
+    def get(self):
+        return flask.render_template("rating.html")
+    def post(self):
+        return 'hiiiiiiiiiii'
+
+app.add_url_rule("/",view_func=View.as_view("main"), methods=['GET','POST'])
+app.add_url_rule("/<page>/",view_func=View.as_view("main"), methods=['GET','POST'])
+app.add_url_rule("/<page1>/",view_func=log.as_view("main"), methods=['GET','POST'])
+app.add_url_rule("/login",view_func=log.as_view("login"), methods=['GET','POST'])
+app.add_url_rule("/rating",view_func=rating.as_view("rating"), methods=['GET','POST'])
+
+app.debug = True
+app.run()
